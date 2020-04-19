cbfc8f965f751c8b699dd92069018e0d58b7af07
Jussi Leinonen <hullcaid@gmail.com>
2020-04-19 19:49:32 +0300

    Tehtävä 2.1

---
+++ osa2/kurssitiedot/src/index.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Course = (props) => {
+  console.log(props)
+  return(
+    <div>
+      <Header course={props.course.name} />
+      <Content parts={props.course.parts} />
+    </div>
+  )
+}
+const Header = (props) => {
+  console.log(props)
+  return(
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  console.log(props)
+  return(
+    <div>
+      {props.parts.map(part => <Part key={part.id} part={part.name} exercises={part.exercises}/>)}
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return(
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
+
+const Total = (props) => {
+  return(
+    <p>
+      Number of Exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
+    </p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'State of a union',
+        exercises: 12,
+        id: 4
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+ReactDOM.render(<App />, document.getElementById('root'))
+;
