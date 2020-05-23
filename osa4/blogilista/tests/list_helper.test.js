821b8436a490dbb71bfd09d04ee8b0a391fa7e79
Jussi Leinonen <hullcaid@gmail.com>
2020-05-23 13:26:59 +0300

    Tehtävä 4.5

--- osa4/blogilista/tests/list_helper.test.js
+++ osa4/blogilista/tests/list_helper.test.js
@@ -9,44 +9,38 @@ describe('dummy', () => {
   })
 })
 
-describe('total likes', () => {
-  const listWithOneBlog = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-      __v: 0
-    }
-  ]
+const listWithOneBlog = [
+  {
+    _id: '5a422aafasdfsdafasdd17f8',
+    title: 'Title0',
+    author: 'Name0',
+    url: 'http://url0.html',
+    likes: 1,
+    __v: 0
+  }
+]
 
-  const listOfMultipleBlogs = [
-    {
-      _id: '5a422aafasdfsdafasdd17f8',
-      title: 'Title0',
-      author: 'Name0',
-      url: 'http://url0.html',
-      likes: 5,
-      __v: 0
-    },
-    {
-      _id: '5a422aa71b54357f8',
-      title: 'Title1',
-      author: 'Name1',
-      url: 'http://url1.html',
-      likes: 3,
-      __v: 0
-    },
-    {
-      _id: '5a422aa71bsdsdfsdsdf17f8',
-      title: 'Title2',
-      author: 'Name2',
-      url: 'http://url2.html',
-      likes: 1,
-      __v: 0
-    }
-  ]
+const listOfMultipleBlogs = [
+  listWithOneBlog[0],
+  {
+    _id: '5a422aa71b54357f8',
+    title: 'Title1',
+    author: 'Name1',
+    url: 'http://url1.html',
+    likes: 3,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71bsdsdfsdsdf17f8',
+    title: 'Title2',
+    author: 'Name2',
+    url: 'http://url2.html',
+    likes: 5,
+    __v: 0
+  }
+]
+
+describe('total likes', () => {
 
   test('of empty list is zero', () => {
     const result = listHelper.totalLikes([])
@@ -61,5 +55,33 @@ describe('total likes', () => {
   test('of a bigger list is calculated right', () => {
     const result = listHelper.totalLikes(listOfMultipleBlogs)
     expect(result).toBe(9)
+  })
+})
+
+describe('favorite blog', () => {
+
+  test('of empty list is empty object', () => {
+    const result = listHelper.favoriteBlog([])
+    expect(result).toEqual({})
+  })
+
+  test('when list has only blog, it is the favorite', () => {
+    const expectedResult = {
+      title: listWithOneBlog[0].title,
+      author: listWithOneBlog[0].author,
+      likes: listWithOneBlog[0].likes
+    }
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    expect(result).toEqual(expectedResult)
+  })
+
+  test('with list of blogs is the most liked one', () => {
+    const expectedResult = {
+      title: listOfMultipleBlogs[2].title,
+      author: listOfMultipleBlogs[2].author,
+      likes: listOfMultipleBlogs[2].likes
+    }
+    const result = listHelper.favoriteBlog(listOfMultipleBlogs)
+    expect(result).toEqual(expectedResult)
   })
 })
--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -13,6 +13,27 @@ const totalLikes = (blogs) => {
   return blogs.length === 0 ? 0 : likes
 }
 
+const favoriteBlog = (blogs) => {
+  if(blogs.length > 0) {
+    const { title, author, likes } = blogs[0]
+    const first = { title: title, author: author, likes: likes}
+
+    const getFavorite = (best, blog) => {
+      if (best.likes < blog.likes) {
+        best.title = blog.title
+        best.author = blog.author
+        best.likes = blog.likes
+      }
+      return best
+    }
+
+    const favorite = blogs.reduce(getFavorite, first)
+
+    return favorite
+  }
+  return {}
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
